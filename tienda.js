404c823d8cdd245140ab28b8415cfa2d42b52bc1
bienve98 <bienvenido98@hotmail.es>
2021-02-23 17:17:26 +0100

    Añado logo carrito e instagram,mejoro el carrito en la tienda

--- tienda.js
+++ tienda.js
@@ -5,24 +5,37 @@ document.addEventListener("DOMContentLoaded",function(){
     const listaCarro= document.querySelector("#listaCarro")
     const tituloBienve = document.querySelector("#tituloBienve")
     const tituloPaco = document.querySelector("#tituloPaco")
+    const precioPaco = document.querySelector("#precioPaco")
+    const precioBienve = document.querySelector("#precioBienve")
+    
 
     BienveBoton.addEventListener("click",function(){
-        addItemToList(tituloBienve,listaCarro)
+        const contenidoBienve=tituloBienve.textContent+" "+precioBienve.textContent
+        addItemToList(contenidoBienve,listaCarro)
     })
 
     PacoBoton.addEventListener("click",function(){
-        addItemToList(tituloPaco,listaCarro)
+        const contenidoPaco=tituloPaco.textContent+" "+precioPaco.textContent
+        addItemToList(contenidoPaco,listaCarro)
     })
 
-
-    function addItemToList(tituloBienve,listaCarro) {
-        console.log(tituloBienve.textContent)
+    function addItemToList(texto,listaCarro) {
+        console.log(texto)
             //crear un nuevo LI con el texto de la caja
             let nuevoLI = document.createElement("LI")
-            nuevoLI.textContent = tituloBienve.textContent.trim()
+            nuevoLI.textContent = texto
             //insertarlo en la lista "My shopping list"
             listaCarro.appendChild(nuevoLI)
         
     }
+
+    $(function () {
+        $('[data-toggle="popover"]').popover({
+          html: true,
+          content: function() {
+              return $('.popover').html();
+            }
+        });
+      })
 
 })

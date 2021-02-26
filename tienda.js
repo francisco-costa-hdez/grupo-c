626cdf309ef9b221970c022b19911ab283ffa9b1
FranciscoCosta <francisco.costa.hdez@gmail.com>
2021-02-26 19:41:19 +0100

    añado login y modifico todas las páginasgit add .git add .git add .git add .

--- tienda.js
+++ tienda.js
@@ -1,41 +1,36 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function() {
 
-    const BienveBoton = document.querySelector("#BienveBoton")
-    const PacoBoton = document.querySelector("#PacoBoton")
-    const listaCarro= document.querySelector("#listaCarro")
+    const btnCart = document.querySelectorAll(".addToCart")
+    const listaCarro = document.querySelector("#listaCarro")
     const tituloBienve = document.querySelector("#tituloBienve")
     const tituloPaco = document.querySelector("#tituloPaco")
     const precioPaco = document.querySelector("#precioPaco")
     const precioBienve = document.querySelector("#precioBienve")
-    
 
-    BienveBoton.addEventListener("click",function(){
-        const contenidoBienve=tituloBienve.textContent+" "+precioBienve.textContent
-        addItemToList(contenidoBienve,listaCarro)
+    btnCart.forEach(item => {
+        item.addEventListener('click', () => {
+            const contenido = item.parentElement.parentElement.firstElementChild.textContent + " " + item.parentElement.parentElement.firstElementChild.nextElementSibling.textContent
+            addItemToList(contenido, listaCarro)
+        })
     })
 
-    PacoBoton.addEventListener("click",function(){
-        const contenidoPaco=tituloPaco.textContent+" "+precioPaco.textContent
-        addItemToList(contenidoPaco,listaCarro)
-    })
-
-    function addItemToList(texto,listaCarro) {
-        console.log(texto)
-            //crear un nuevo LI con el texto de la caja
-            let nuevoLI = document.createElement("LI")
-            nuevoLI.textContent = texto
+    function addItemToList(texto, listaCarro) {
+        //crear un nuevo LI con el texto de la caja
+        let nuevoLI = document.createElement("LI")
+        nuevoLI.classList.add("list-group-item")
+        nuevoLI.textContent = texto
             //insertarlo en la lista "My shopping list"
-            listaCarro.appendChild(nuevoLI)
-        
+        listaCarro.prepend(nuevoLI)
+
     }
 
-    $(function () {
+    $(function() {
         $('[data-toggle="popover"]').popover({
-          html: true,
-          content: function() {
-              return $('.popover').html();
+            html: true,
+            content: function() {
+                return $('.popover').html();
             }
         });
-      })
+    })
 
 })
